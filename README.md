20e0e2398628ff4b832883a025a76051620723d3
Seth Holladay <me@seth-holladay.com>
2016-09-07 19:16:03 -0400

    init

---
+++ README.md
@@ -0,0 +1,112 @@
+# scube [![Build status for scube on Circle CI.](https://img.shields.io/circleci/project/sholladay/scube/master.svg "Circle Build Status")](https://circleci.com/gh/sholladay/scube "Scube Builds")
+
+> Manage your S3 buckets.
+
+## Why?
+
+ - High-level, promise-based API.
+ - Fixes the quirks of the [official SDK](https://github.com/aws/aws-sdk-js).
+ - Enforces best practices and sensible defaults.
+
+## Install
+
+```sh
+npm install scube --save
+```
+
+## Usage
+
+Get it into your program.
+
+```js
+const Scube = require('scube');
+```
+
+Create a new S3 client.
+
+```js
+const myBucket = new Scube({
+    bucket : 'my-bucket'
+});
+```
+
+Create a bucket.
+
+```js
+myBucket.createBucket().then((data) => {
+    console.log('Bucket created:', data.location);
+});
+```
+
+You can also work with directories as first-class citizens.
+
+```js
+myBucket.listDir({ prefix : 'foo' }).then((data) => {
+    console.log('Directory contents:', data.contents);
+})
+```
+
+Delete a directory.
+
+```js
+myBucket.deleteDir({ prefix : 'foo' }).then((data) => {
+    console.log('Directory deleted:', data.prefix);
+});
+```
+
+## API
+
+Please see Amazon's [API documentation](http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html) for details on the option and response properties.
+
+### Scube(option)
+
+#### option
+
+Type: `object`
+
+Default configuration for all client actions.
+
+##### bucket
+
+Type: `string`
+
+The default bucket name to use. This is a global (worldwide) namespace to store your data in.
+
+##### delimiter
+
+Type: `string`<br>
+Default: `/`
+
+The default delimiter character to use. Helpful to group together keys starting with a `prefix` not followed by a `delimiter`.
+
+### Instance
+
+Most of the official SDK methods are exposed via an equivalent promise-based counterpart. The ones below are unique to this library.
+
+#### .listDir(option)
+
+List all keys within the `option.prefix` directory.
+
+#### .deletePrefix(option)
+
+Delete all keys that start with `option.prefix`.
+
+#### .deleteDir(option)
+
+Delete all keys within the `option.prefix` directory.
+
+## Contributing
+
+See our [contributing guidelines](https://github.com/sholladay/scube/blob/master/CONTRIBUTING.md "The guidelines for participating in this project.") for more details.
+
+1. [Fork it](https://github.com/sholladay/scube/fork).
+2. Make a feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. [Submit a pull request](https://github.com/sholladay/scube/compare "Submit code to this project for review.").
+
+## License
+
+[MPL-2.0](https://github.com/sholladay/scube/blob/master/LICENSE "The license for scube.") © [Seth Holladay](http://seth-holladay.com "Author of scube.")
+
+Go make something, dang it.
---
+++ index.js
@@ -0,0 +1,217 @@
+'use strict';
+
+const { S3 } = require('aws-sdk');
+
+const makeCallback = (resolve, reject) => {
+    return (err, data) => {
+        if (err) {
+            reject(err);
+            return;
+        }
+        resolve(data);
+    };
+};
+
+const capitalize = (str) => {
+    return str[0].toUpperCase() + str.substring(1);
+};
+
+const decapitalize = (str) => {
+    return str[0].toLowerCase() + str.substring(1);
+};
+
+// Map object keys to the unconventional format expected by the AWS SDK.
+const capKeys = (obj) => {
+    if (!obj) {
+        return obj;
+    }
+
+    const target = {};
+    Object.keys(obj).forEach((key) => {
+        target[capitalize(key)] = obj[key];
+    });
+
+    return target;
+};
+
+const decapKeys = (obj) => {
+    if (!obj) {
+        return obj;
+    }
+
+    const target = {};
+    Object.keys(obj).forEach((key) => {
+        target[decapitalize(key)] = obj[key];
+    });
+
+    return target;
+};
+
+class Scube {
+    constructor(option) {
+        const config = Object.assign(
+            {
+                region    : 'us-east-1',
+                delimiter : '/'
+            },
+            option
+        );
+
+        if (!config.bucket) {
+            throw new Error('A bucket name is required.');
+        }
+
+        this._service = new S3({
+            region : config.region,
+            // s3ForcePathStyle : true,
+            // sslEnabled : true,
+            // signatureVersion : 'v4',
+            params : capKeys({
+                bucket    : config.bucket,
+                delimiter : config.delimiter
+            })
+        });
+    }
+
+    copyObject(param) {
+        return new Promise((resolve, reject) => {
+            this._service.copyObject(capKeys(param), makeCallback(resolve, reject));
+        });
+    }
+
+    createBucket(param) {
+        return new Promise((resolve, reject) => {
+            this._service.createBucket(capKeys(param), makeCallback(resolve, reject));
+        }).then(decapKeys);
+    }
+
+    deleteBucket(param) {
+        return new Promise((resolve, reject) => {
+            this._service.deleteBucket(capKeys(param), makeCallback(resolve, reject));
+        });
+    }
+
+    deleteObject(param) {
+        return new Promise((resolve, reject) => {
+            this._service.deleteObject(capKeys(param), makeCallback(resolve, reject));
+        });
+    }
+
+    deleteObjects(param) {
+        return new Promise((resolve, reject) => {
+            this._service.deleteObjects(capKeys(param), makeCallback(resolve, reject));
+        }).then(decapKeys);
+    }
+
+    getObject(param) {
+        return new Promise((resolve, reject) => {
+            this._service.getObject(capKeys(param), makeCallback(resolve, reject));
+        });
+    }
+
+    getSignedUrl(operation, param) {
+        return new Promise((resolve, reject) => {
+            this._service.getSignedUrl(operation, capKeys(param), makeCallback(resolve, reject));
+        });
+    }
+
+    headBucket(param) {
+        return new Promise((resolve, reject) => {
+            this._service.headBucket(capKeys(param), makeCallback(resolve, reject));
+        });
+    }
+
+    headObject(param) {
+        return new Promise((resolve, reject) => {
+            this._service.headObject(capKeys(param), makeCallback(resolve, reject));
+        });
+    }
+
+    listBuckets(param) {
+        return new Promise((resolve, reject) => {
+            this._service.listBuckets(capKeys(param), makeCallback(resolve, reject));
+        });
+    }
+
+    listObjects(param) {
+        return new Promise((resolve, reject) => {
+            this._service.listObjectsV2(capKeys(param), makeCallback(resolve, reject));
+        }).then(decapKeys);
+    }
+
+    putObject(param) {
+        return new Promise((resolve, reject) => {
+            this._service.putObject(capKeys(param), makeCallback(resolve, reject));
+        });
+    }
+
+    upload(param, option) {
+        return new Promise((resolve, reject) => {
+            this._service.upload(capKeys(param), option, makeCallback(resolve, reject));
+        }).then(decapKeys);
+    }
+
+    // Custom methods.
+
+    deletePrefix(input) {
+        const option = Object.assign({}, input, {
+            // List all individual objects under the prefix.
+            delimiter : ''
+        });
+        return new Promise((resolve, reject) => {
+            const go = (override) => {
+                const config = Object.assign(option, override);
+                let list;
+                this.listObjects(config)
+                    .then((res) => {
+                        list = res;
+                        if (!list.contents.length) {
+                            return;
+                        }
+                        return this.deleteObjects({
+                            delete : capKeys({
+                                objects : list.contents.map((obj) => {
+                                    return capKeys({
+                                        key : obj.Key
+                                    });
+                                })
+                            })
+                        });
+                    })
+                    .then(() => {
+                        if (list.isTruncated) {
+                            go({ continuationToken : list.nextContinuationToken });
+                            return;
+                        }
+                        resolve();
+                    })
+                    .catch((err) => {
+                        reject(err);
+                    });
+            };
+            go();
+        });
+    }
+
+    deleteDir(option) {
+        const config = Object.assign({}, option);
+
+        if (!config.prefix.endsWith('/')) {
+            config.prefix += '/';
+        }
+
+        return this.deletePrefix(config);
+    }
+
+    listDir(option) {
+        const config = Object.assign({}, option);
+
+        if (!config.prefix.endsWith('/')) {
+            config.prefix += '/';
+        }
+
+        return this.listObjects(config);
+    }
+}
+
+module.exports = Scube;
