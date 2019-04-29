e5920c11dcbadd159e45e044c9970c6e82a55282
Seth Holladay <me@seth-holladay.com>
2019-04-29 15:17:12 -0400

    Tweak code style

--- index.js
+++ index.js
@@ -19,13 +19,13 @@ const capKeys = (obj) => {
 class Scube {
     constructor(option) {
         const config = joi.attempt(option, joi.object().required().keys({
-            region           : joi.string().default('us-east-1'),
-            delimiter        : joi.string().default('/'),
+            region           : joi.string().optional().default('us-east-1'),
+            delimiter        : joi.string().optional().default('/'),
             bucket           : joi.string().required().hostname().min(1),
             publicKey        : joi.string().required().token().min(20),
             secretKey        : joi.string().required().base64().min(40),
             endpoint         : joi.string().optional(),
-            s3ForcePathStyle : joi.boolean().default(false).optional()
+            s3ForcePathStyle : joi.boolean().optional().default(false)
         }));
 
         this.s3 = new S3({
